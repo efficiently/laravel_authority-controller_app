830cb8e7fafd40c671099a51f8a97c139ada7595
Tortue Torche <tortuetorche@spam.me>
2013-12-20 16:49:27 +0100

    Show the users list link on the home page if you are authorized to read it.

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.application')
 
 @section('content')
-<h2>Welcome</h2>
+  <h2>Welcome</h2>
+  @if (Authority::can('read', 'User'))
+    {{ HTML::linkRoute('users.index', "See users list") }}
+  @endif
 @stop
