744f12bc741b22cc13e2841c747636add32d849b
Tortue Torche <tortuetorche@spam.me>
2015-02-23 18:49:23 +0100

    Reset to a new Laravel 5 application.

--- app/views/home.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.application')
-
-@section('content')
-  <h2>Welcome</h2>
-  @if (Authority::can('read', 'User'))
-    {{ HTML::linkRoute('users.index', "See users list") }}
-  @endif
-@stop
